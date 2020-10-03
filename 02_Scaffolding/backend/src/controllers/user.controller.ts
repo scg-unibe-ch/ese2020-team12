5cb28ab77b7ee4f2e495ff817a1ae3a9a5041fc8
Marko Cirkovic <marko.cirkovic@students.unibe.ch>
2020-10-03 23:49:28 +0200

    Registration funktioniert

--- 02_Scaffolding/backend/src/controllers/user.controller.ts
+++ 02_Scaffolding/backend/src/controllers/user.controller.ts
@@ -2,23 +2,32 @@
 import express, { Router, Request, Response } from 'express';
 import { UserService } from '../services/user.service';
 import { verifyToken } from '../middlewares/checkAuth';
-import {isUsernameUnique, isEmailUnique} from '../middlewares/checkReg';
+import {User} from '../models/user.model';
+import {Op} from 'sequelize';
 
 const userController: Router = express.Router();
 const userService = new UserService();
 
 userController.post('/register',
     (req: Request, res: Response) => {
-        if (isUsernameUnique(req)) {
-            if (isEmailUnique(req)) {
-                userService.register(req.body).then(registered => res.send(registered)).catch(err => res.status(500).send(err));
+        return User.findOne({
+            where: {
+                [Op.or]: [
+                    {userName: req.body.userName},
+                    {email : req.body.email}
+                ]
             }
-            res.status(333).send('the email is already used');
-        }
-        res.status(333).send('the username is already used');
-    }
-);
+        })
+            .then(user => {
+                if (user == null) {
+                    userService.register(req.body).then(registered => res.send(registered));
+                } else {
+                    res.send('Username or Email is already used!');
+                }
+            }).catch(err => Promise.reject({ message: err }));
+    });
 
+// userService.register(req.body).then(registered => res.send(registered)).catch(err => res.status(500).send(err));
 userController.post('/login',
     (req: Request, res: Response) => {
         userService.login(req.body).then(login => res.send(login)).catch(err => res.status(500).send(err));
