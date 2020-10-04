48288886c9a946dc69ad3d878207ed1d8ba6deab
philipp-ueltschi <ueltschi.philipp@gmail.com>
2020-10-04 13:51:00 +0200

    file exchange frontend

--- 02_Scaffolding/frontend/src/app/app.module.ts
+++ 02_Scaffolding/frontend/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
