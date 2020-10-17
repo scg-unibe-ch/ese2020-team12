4dc8cf9ee7343e030199bde76bb8622ee842d6fa
Maurizio Piu <maurizio.piu@students.unibe.ch>
2020-10-17 12:26:23 +0200

    Added all the necessary input fields of the addArticle component. conditionals not included yet

--- 02_Scaffolding/frontend/src/app/add-article/add-article.component.ts
+++ 02_Scaffolding/frontend/src/app/add-article/add-article.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule} from '@angular/forms';
-import {ActivatedRoute} from "@angular/router";
-import {HttpClient} from "@angular/common/http";
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../environments/environment';
 
 @Component({
   selector: 'app-add-article',
@@ -13,18 +14,22 @@ export class AddArticleComponent implements OnInit {
   articleType;
   title;
   price;
-  pricePer;
+  pricingType;
   description;
   location;
   sellOrLend;
   lendingStatus;
-  deliveryPossible;
-  deliveryCost;
+  delivery;
+  deliverySpecs;
   expenses;
   expensesCost;
 
   addArticleForm: FormGroup;
   articleTypes: any = [ 'Product', 'Service'];
+  pricingTypes: any = [ 'per hour', 'per day'];
+  distribution: any = [ 'Sell', 'Lend'];
+  lendingStates: any = [ 'Available', 'Lent'];
+  deliveryOptions: any = [ 'Yes', 'No'];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -48,7 +53,7 @@ export class AddArticleComponent implements OnInit {
         [Validators.required]
         )),
 
-    })
+    });
   }
 
   ngOnInit(): void {
@@ -58,10 +63,63 @@ export class AddArticleComponent implements OnInit {
 
   }
 
-
-  changeType(e) {
+  changeArticleType(e: any): void {
     this.articleType.setValue(e.target.value, {
       onlySelf: true
     });
   }
+
+  changePricingType(e: any): void {
+    this.pricingType.setValue(e.target.value, {
+      onlySelf: true
+    });
+  }
+
+  changeDistroType(e: any): void {
+    this.sellOrLend.setValue(e.target.value, {
+      onlySelf: true
+    });
+  }
+
+  changeLendingStatus(e: any): void {
+    this.lendingStatus.setValue(e.target.value, {
+      onlySelf: true
+    });
+  }
+
+  changeDeliveryOption(e: any): void {
+    this.delivery.setValue(e.target.value, {
+      onlySelf: true
+    });
+  }
+
+  /*
+  sendForm(): void {
+    this.httpClient.post(environment.endpointURL + 'user/signup', {
+      name: this.name,
+      surname: this.surname,
+      userName: this.username,
+      email: this.email,
+      street: this.street,
+      houseNumber: this.houseNumber,
+      place: this.city,
+      postalCode: this.postalCode,
+      password: this.password
+    }).subscribe((res: any) => {
+      // Set user data in local storage
+      localStorage.setItem('name', res.name);
+      localStorage.setItem('surname', res.surname);
+      localStorage.setItem('userName', res.userName);
+      localStorage.setItem('email', res.email);
+      localStorage.setItem('street', res.street);
+      localStorage.setItem('houseNumber', res.houseNumber);
+      localStorage.setItem('place', res.place);
+      localStorage.setItem('postalCode', res.postalCode);
+      localStorage.setItem('password', res.password);
+    });
+  }
+   */
+
+
+
 }
