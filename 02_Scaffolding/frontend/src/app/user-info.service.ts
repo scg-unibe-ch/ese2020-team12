07e1db6badefe40cf6f68800573ae3a5782b9e9e
Marko Cirkovic <marko.cirkovic@students.unibe.ch>
2020-11-04 16:33:29 +0100

    service frontend

--- 02_Scaffolding/frontend/src/app/user-info.service.ts
+++ 02_Scaffolding/frontend/src/app/user-info.service.ts
@@ -1,8 +1,8 @@
-/* tslint:disable */
+
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 import {environment} from '../environments/environment';
 import {HttpClient} from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +15,8 @@ export class UserInfoService {
   username: string;
   userToken: any; // change to correct type
   password: string;
+  userId: string;
+  user: any;
 
   email: string;
   name: string;
@@ -24,39 +26,19 @@ export class UserInfoService {
   postalCode: number;
   place: string;
 
-  // Profile Stream
-  profile$ = this.httpClient.get(environment.endpointURL + 'user/profile');
+  getUser(): void {
+    this.httpClient.get(environment.endpointURL + 'user/profile/' + this.userId)
+      .subscribe((res: any ) => {
+        this.user = res;
+      });
+  }
 
-  // Getters
-  getIsLoggedIn(): boolean{ return this.isLoggedIn; }
-  getUsername(): string{ return this.username; }
-  getUserToken(): any{ return this.userToken; }
-  getPassword(): string{ return this.password; }
-  getEmail(): string{ return this.email; }
-  getName(): string{ return this.name; }
-  getSurname(): string{ return this.surname; }
-  getStreet(): string{ return this.street; }
-  getHouseNumber(): number{ return this.houseNumber; }
-  getPostalCode(): number{ return this.postalCode; }
-  getPlace(): string{ return this.place; }
 
   constructor(
     private httpClient: HttpClient
   ) {
-    // Observers
-    this.profile$.subscribe((res: any) => this.isLoggedIn = res.loggedIn);
-    // does res.username needs camelcase?
-    this.profile$.subscribe((res: any) => this.username = res.username);
-    this.profile$.subscribe((res: any) => this.userToken = res.userToken);
-    this.profile$.subscribe((res: any) => this.password = res.password);
-
-    this.profile$.subscribe((res: any) => this.email = res.email);
-    this.profile$.subscribe((res: any) => this.name = res.name);
-    this.profile$.subscribe((res: any) => this.street = res.street);
-    this.profile$.subscribe((res: any) => this.houseNumber = res.houseNumber);
-    this.profile$.subscribe((res: any) => this.postalCode = res.postalCode);
-    this.profile$.subscribe((res: any) => this.place = res.place);
-
+    this.getUser();
+    console.log(this.user);
   }
 
   // Where do i need to put this? How do i need to write this? (30.10.20 -mauri)
@@ -65,3 +47,16 @@ export class UserInfoService {
   }
 
 }
+
+/*
+ this.getUserDate();
+    console.log(this.username,
+      this.password,
+      this.email,
+      this.name,
+      this.surname,
+      this.street,
+      this.houseNumber,
+      this.postalCode,
+      this.place);
+ */
