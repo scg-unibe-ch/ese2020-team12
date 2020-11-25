9b5a4e004341949424965b71e8dcd7f825b28876
philipp-ueltschi <ueltschi.philipp@gmail.com>
2020-11-25 19:30:18 +0100

    worked on add article pages to combine all three into one page

--- 02_Scaffolding/frontend/src/app/add-article/add-article.component.ts
+++ 02_Scaffolding/frontend/src/app/add-article/add-article.component.ts
@@ -3,11 +3,31 @@ import {FormBuilder, FormControl, FormGroup, Validators, ReactiveFormsModule} fr
 import {ActivatedRoute} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import {UserInfoService} from '../user-info.service';
 
 
 interface ArticleType{
   text: string;
   link: string;
+}
+
+interface Delivery {
+  value: boolean;
+  viewValue: string;
+}
+
+interface Category {
+  name: string;
+  id: number;
+}
+
+interface Pricing {
+  value: string;
+}
+
+interface LendingStatus {
+  value: boolean;
+  viewValue: string;
 }
 
 @Component({
@@ -17,7 +37,32 @@ interface ArticleType{
 })
 export class AddArticleComponent implements OnInit {
 
-  articleForm;
+  addArticleForm;
+  sellTitle;
+  sellPrice;
+  sellCategory;
+  sellDescription;
+  sellLocation;
+  sellDelivery;
+  sellDeliverySpecs;
+
+  lendTitle;
+  lendPrice;
+  lendPricingType;
+  lendCategory;
+  lendDescription;
+  lendLocation;
+  lendLendingStatus;
+  lendHandling;
+
+  serviceTitle;
+  servicePrice;
+  servicePricingType;
+  serviceCategory;
+  serviceDescription;
+  serviceLocation;
+  serviceExpenses;
+  serviceExpensesCost;
 
   articleTypes: ArticleType[] = [
     {text: 'sell a product', link: '/add-article/sell-product'},
@@ -25,42 +70,87 @@ export class AddArticleComponent implements OnInit {
     {text: 'provide a service', link: '/add-article/provide-service'}
   ];
 
+  // addArticleForm: FormGroup;
+  deliveryOptions: Delivery[] = [
+    {value: true, viewValue: 'Yes'},
+    {value: false, viewValue: 'No'}
+  ];
+
+  pricingOptions: Pricing[] = [
+    {value: 'per hour'},
+    {value: 'per day'}
+  ];
+
+  statusOptions: LendingStatus[] = [
+    {value: true, viewValue: 'Available'},
+    {value: false, viewValue: 'Unavailable'}
+  ];
+
+  // change id with accordance to backend
+  categories: Category[] = [
+    {name: 'clothes/shoes', id: 1},
+    {name: 'electronics', id: 2},
+    {name: 'household/garden', id: 3},
+    {name: 'real estate', id: 4},
+    {name: 'sport', id: 5},
+    {name: 'vehicles', id: 6},
+  ];
+
   constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private httpClient: HttpClient,
+    private userInfoService: UserInfoService
   ) {
-    this.articleForm = new FormControl('', Validators.compose([Validators.required]));
+    // this.articleForm = new FormControl('', Validators.compose([Validators.required]))
+    this.addArticleForm = this.formBuilder.group({
+      title: new FormControl('', Validators.compose(
+        [Validators.required]
+      )),
+      price: new FormControl('', Validators.compose(
+        [Validators.required]
+      )),
+      category: new FormControl('', Validators.compose(
+        [Validators.required]
+      )),
+      description: new FormControl('', Validators.compose(
+        [Validators.required]
+      )),
+      location: new FormControl('', Validators.compose(
+        [Validators.required]
+      )),
+      delivery: new FormControl('', Validators.compose(
+        [Validators.required]
+      )),
+      deliverySpecs: new FormControl('', Validators.compose(
+        [Validators.required]
+      ))
+    });
   }
 
   ngOnInit(): void {
   }
 
-
-  /*
-  // only copy-pasted method from the signupForm, needs to be updated
   addArticleRequest(): void {
-    this.httpClient.post(environment.endpointURL + 'user/add-article', {
-      name: this.name,
-      surname: this.surname,
-      userName: this.username,
-      email: this.email,
-      street: this.street,
-      houseNumber: this.houseNumber,
-      place: this.city,
-      postalCode: this.postalCode,
-      password: this.password
+    this.httpClient.post(environment.endpointURL + 'add-article/sell-product/', {
+      title: this.sellTitle,
+      price: this.sellPrice,
+      category: this.sellCategory,
+      description: this.sellDescription,
+      location: this.sellLocation,
+      delivery: this.sellDelivery,
+      delSpec: this.sellDeliverySpecs,
+      userId: this.userInfoService.getUserId()
     }).subscribe((res: any) => {
       // Set user data in local storage
-      localStorage.setItem('name', res.name);
-      localStorage.setItem('surname', res.surname);
-      localStorage.setItem('userName', res.userName);
-      localStorage.setItem('email', res.email);
-      localStorage.setItem('street', res.street);
-      localStorage.setItem('houseNumber', res.houseNumber);
-      localStorage.setItem('place', res.place);
-      localStorage.setItem('postalCode', res.postalCode);
-      localStorage.setItem('password', res.password);
+      // we dont need that, right? (mauri 13.11)
+      localStorage.setItem('title', res.title);
+      localStorage.setItem('price', res.price);
+      localStorage.setItem('description', res.description);
+      localStorage.setItem('location', res.location);
+      localStorage.setItem('delivery', res.delivery);
+      localStorage.setItem('delSpec', res.delSpec);
     });
   }
-  */
-
 
 }
