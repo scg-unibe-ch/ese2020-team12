98032cd9f37e26b367270203dd0d73bc7a83f41b
philipp-ueltschi <ueltschi.philipp@gmail.com>
2020-11-28 13:43:00 +0100

    added articles.status to services

--- 02_Scaffolding/frontend/src/app/article-page/article-page.component.ts
+++ 02_Scaffolding/frontend/src/app/article-page/article-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ArticleInfoService} from '../article-info.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 
 @Component({
@@ -11,13 +12,19 @@ export class ArticlePageComponent implements OnInit {
 
   public article: any;
 
+
   constructor(
     public articleInfoService: ArticleInfoService,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
     this.article = this.articleInfoService.getArticle();
-
   }
 
+  openSnackBar(message: string, action: string): void{
+    this.snackBar.open(message, action, {
+      duration: 4000,
+    });
+  }
 }
