e9bf676ae4f9db23463d75be658dd9bd996574e8
philipp-ueltschi <ueltschi.philipp@gmail.com>
2020-11-29 18:58:40 +0100

    worked on shopping cart page and inserted link to shopping cart to topbar menue

--- 02_Scaffolding/frontend/src/app/shoppingcart/shoppingcart.component.ts
+++ 02_Scaffolding/frontend/src/app/shoppingcart/shoppingcart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ShoppingCartService} from '../shopping-cart.service';
 import {UserInfoService} from '../user-info.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-shoppingcart',
@@ -9,12 +10,28 @@ import {UserInfoService} from '../user-info.service';
 })
 export class ShoppingcartComponent implements OnInit {
 
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  thirdFormGroup: FormGroup;
+  otherAddress: boolean;
+  isOptional = false;
+
   constructor(
     public shoppingCartService: ShoppingCartService,
-    private userInfoService: UserInfoService
+    private userInfoService: UserInfoService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.firstFormGroup = this.formBuilder.group({
+      firstCtrl: ''
+    });
+    this.secondFormGroup = this.formBuilder.group({
+      secondCtrl: ''
+    });
+    this.thirdFormGroup = this.formBuilder.group({
+      secondCtrl: ''
+    });
     this.userInfoService.getUserFromBackend();
     setTimeout(() =>
       {
@@ -23,6 +40,12 @@ export class ShoppingcartComponent implements OnInit {
       1000);
   }
 
+  changeOtherAddress(): void {
+    this.otherAddress = true;
+  }
 
+  changeOtherAddress2(): void {
+    this.otherAddress = false;
+  }
 
 }
