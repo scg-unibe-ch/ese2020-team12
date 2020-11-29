9b21b72dde7db63022e63a52814e2ddff4229288
Marko Cirkovic <marko.cirkovic@students.unibe.ch>
2020-11-29 17:10:07 +0100

    delete-methode backend

--- 02_Scaffolding/backend/src/controllers/shoppingCart.controller.ts
+++ 02_Scaffolding/backend/src/controllers/shoppingCart.controller.ts
@@ -1,5 +1,7 @@
 import express, {Request, Response, Router} from 'express';
 import {ShoppingCartService} from '../services/shoppingCart.service';
+import {User} from '../models/user.model';
+import {ShoppingCartItem} from '../models/shoppingCart.model';
 
 
 
@@ -21,6 +23,32 @@ shoppingCartController.get('/:id', (req: Request, res: Response) => {
     shoppingCartService.getSellArticles(Number(req.params.id))
         .then(list => res.status(200).send(list)).catch(err => res.status(500).send(err));
 });
+
+
+shoppingCartController.delete('/delete/item/:cartId', (req, res) => {
+    ShoppingCartItem.findByPk(req.params.cartId)
+        .then(found => {
+            if (found != null) {
+                found.destroy()
+                    .then(item => res.status(200).send({ deleted: item }))
+                    .catch(err => res.status(500).send(err));
+            } else {
+                res.sendStatus(404);
+            }
+        }).catch(err => res.status(500).send(err));
+});
+
+shoppingCartController.delete('/delete/:userId', (req, res) => {
+    ShoppingCartItem.destroy({
+        where: {
+            userId: req.params.userId
+        }
+    }).then(item => {
+        res.status(200).send({ deleted: item });
+        }).
+    catch(err => res.status(500).send(err));
+});
+
 
 
 
